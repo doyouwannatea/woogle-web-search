d4fc173ae6f910cfa42507071d15a1c91841d361
Alexandr Bulgatov <alexandr.bulgatov13@gmail.com>
2022-05-16 16:26:08 +0800

    + поиск при вводе в инпут

--- app/src/main/java/com/example/woogle/FirstFragment.kt
+++ app/src/main/java/com/example/woogle/FirstFragment.kt
@@ -5,10 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.woogle.adapters.WebResultAdapter
 import com.example.woogle.databinding.FragmentFirstBinding
+import com.example.woogle.models.SearchResult
 import com.example.woogle.models.WebSearchResponce
 import com.example.woogle.retrofit.common.Common
 import com.example.woogle.retrofit.interfaces.RetrofitServices
@@ -22,6 +24,8 @@ class FirstFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var mService: RetrofitServices
+    private var searchResultList: MutableList<SearchResult>? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,15 +36,19 @@ class FirstFragment : Fragment() {
         mService = Common.retrofitService
         binding.recyclerView.layoutManager = LinearLayoutManager(context)
 
-        webSearch()
         return binding.root
     }
 
-    private fun webSearch() {
-        mService.webSearch("putin", 1, 10).enqueue(object : Callback<WebSearchResponce> {
+    private fun webSearch(q: String) {
+        searchResultList?.clear()
+        binding.recyclerView.adapter?.notifyDataSetChanged()
+        binding.textViewLoading.visibility = View.VISIBLE
+
+        mService.webSearch(q, 1, 10).enqueue(object : Callback<WebSearchResponce> {
             override fun onFailure(call: Call<WebSearchResponce>, t: Throwable) {
                 Log.e("webSearch", "onFailure")
                 Log.e("webSearch", t.message!!)
+                binding.textViewLoading.visibility = View.INVISIBLE
             }
 
             override fun onResponse(
@@ -48,19 +56,28 @@ class FirstFragment : Fragment() {
                 response: Response<WebSearchResponce>
             ) {
                 Log.d("webSearch", "onResponse")
-                val searchResultList = response.body()?.value
+                searchResultList = response.body()?.value
                 if (searchResultList != null) {
-                    val adapter = WebResultAdapter(requireContext(), searchResultList)
-                    adapter.notifyDataSetChanged()
+                    val adapter = WebResultAdapter(requireContext(), searchResultList!!)
                     binding.recyclerView.adapter = adapter
                 }
+                binding.textViewLoading.visibility = View.INVISIBLE
             }
         })
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.wSearchEditText.setOnEditorActionListener { v, actionId, _ ->
+            return@setOnEditorActionListener when (actionId) {
+                EditorInfo.IME_ACTION_SEARCH -> {
+                    webSearch(v.text.toString().trim())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
